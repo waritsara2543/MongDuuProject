0f5f722242d2dfc19f2cfacaffd0b3ec41afebe6
= <=>
2020-10-10 16:59:47 +0700

    begin category page

---
+++ js/app.js
@@ -0,0 +1,28 @@
+$(function() {
+
+    document.addEventListener('init', function(event) {
+        var page = event.target;
+        
+        console.log(page.id);
+  
+        if (page.id === 'Home') {
+            $('#back').hide();
+            page.querySelector('#category').onclick = function() {
+                document.querySelector('#myNavigator').pushPage('views/category.html');
+              
+            };
+            
+        } else if (page.id === 'category' ) {
+            
+            $('#back').show();
+            document.querySelector('ons-back-button').onclick = function(event) {
+                document.querySelector('#myNavigator').popPage();
+                $('#back').hide();
+            };
+        }
+        
+          
+        
+  
+    });
+  })
