8f5e5288328290603c609d0890f0e4834840f0fa
Rusfirdao <fiirdaoo@gmail.com>
2020-10-12 21:07:36 +0700

    ad loading page

--- js/detail.js
+++ js/detail.js
@@ -20,15 +20,10 @@ $(function(){
     var db = firebase.firestore();
     db.collection("DetailMovie").get().then((querySnapshot) => {
       querySnapshot.forEach((doc) => {
-         var card =`<div class="card ">
-         <img class="card-img-top" src="${doc.data().PosterURL}" alt="">
-         <div class="card-body">
-  <h4 class="card-title">${doc.data().Title} - ${doc.data().Year} </h4>
-  <p class="card-text">${doc.data().Type}</p>
-  <p class="card-text">${doc.data().Detail}</p>
-</div>
-     </div>`;
-     $("#list").append(card);
+         var card =`
+         <div class=""><img class="card-img-top" src="${doc.data().PosterURL}" alt=""></div>
+         `;
+     $("#home").append(card);
   
       });
   });
@@ -50,4 +45,8 @@ $(function(){
 
   })
   //ใส่คำอธิบาย
- 
+//   <div class="card-body">
+//   <h4 class="card-title">${doc.data().Title} - ${doc.data().Year} </h4>
+//   <p class="card-text">${doc.data().Type}</p>
+//   <p class="card-text">${doc.data().Detail}</p>
+// </div>
