301ca72c5cc56d057ecd948e35a61d9e7cdfb2f1
Warissara Wichiansrang <warissara.0039@gmail.com>
2020-10-13 15:25:10 +0700

    ดึงข้อมูลจาก firebase มาใช้ในหน้า category

--- js/detail.js
+++ js/detail.js
@@ -1,26 +1,26 @@
 
-$(function(){
+$(function () {
   // Your web app's Firebase configuration
-    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-    var firebaseConfig = {
-      apiKey: "AIzaSyBfgwFZDoLIRIEIbqayesMIsk4C2BUlvvk",
-      authDomain: "mongduu-cae06.firebaseapp.com",
-      databaseURL: "https://mongduu-cae06.firebaseio.com",
-      projectId: "mongduu-cae06",
-      storageBucket: "mongduu-cae06.appspot.com",
-      messagingSenderId: "19158763880",
-      appId: "1:19158763880:web:1ba86090c57aa8edd5aedd",
-      measurementId: "G-D1SS39EJBZ"
-    };
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
-    firebase.analytics();
-    
-  
-    var db = firebase.firestore();
-    db.collection("DetailMovie").get().then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-         var card =`<div class="card ">
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyBfgwFZDoLIRIEIbqayesMIsk4C2BUlvvk",
+    authDomain: "mongduu-cae06.firebaseapp.com",
+    databaseURL: "https://mongduu-cae06.firebaseio.com",
+    projectId: "mongduu-cae06",
+    storageBucket: "mongduu-cae06.appspot.com",
+    messagingSenderId: "19158763880",
+    appId: "1:19158763880:web:1ba86090c57aa8edd5aedd",
+    measurementId: "G-D1SS39EJBZ"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+
+  var db = firebase.firestore();
+  db.collection("DetailMovie").get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      var card = `<div class="card ">
          <img class="card-img-top" src="${doc.data().PosterURL}" alt="">
          <div class="card-body">
   <h4 class="card-title">${doc.data().Title} - ${doc.data().Year} </h4>
@@ -28,26 +28,142 @@ $(function(){
   <p class="card-text">${doc.data().Detail}</p>
 </div>
      </div>`;
-     $("#list").append(card);
-  
-      });
+      $("#list").append(card);
+
+    });
   });
 
   db.collection("DetailMovie").get().then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
-       var card =`<div class="card ">
+      var card = `<div class="card ">
        <img class="card-img-top" src="${doc.data().PosterURL}" alt=""> <div class="card-body">
        <h4 class="card-title">${doc.data().Title} - ${doc.data().Year} </h4>
        <p class="card-text">${doc.data().Type}</p>
-       <p class="card-text">${doc.data().Detail}</p>
      </div>
       
    </div>`;
-   $("#allcategory").append(card);
+      $("#allcategory").append(card);
 
     });
-});
+  });
 
-  })
+
+
+  db.collection("DetailMovie").where("Type", "==", "ความรัก")
+    .get()
+    .then(function (querySnapshot) {
+      querySnapshot.forEach((doc) => {
+        var card1 = `<div class="card ">
+        <img class="card-img-top" src="${doc.data().PosterURL}" alt=""> <div class="card-body">
+        <h4 class="card-title">${doc.data().Title} - ${doc.data().Year} </h4>
+         <p class="card-text">${doc.data().Type}</p>
+      </div>
+       
+    </div>`;
+        $("#romantic").append(card1);
+
+      });
+    })
+    .catch(function (error) {
+      console.log("Error getting documents: ", error);
+    });
+
+  db.collection("DetailMovie").where("Type", "==", "คอมเมดี้")
+    .get()
+    .then(function (querySnapshot) {
+      querySnapshot.forEach((doc) => {
+        var card1 = `<div class="card ">
+        <img class="card-img-top" src="${doc.data().PosterURL}" alt=""> <div class="card-body">
+        <h4 class="card-title">${doc.data().Title} - ${doc.data().Year} </h4>
+         <p class="card-text">${doc.data().Type}</p>
+      </div>
+       
+    </div>`;
+        $("#comedy").append(card1);
+
+      });
+    })
+    .catch(function (error) {
+      console.log("Error getting documents: ", error);
+    });
+
+  db.collection("DetailMovie").where("Type", "==", "แฟนตาซี")
+    .get()
+    .then(function (querySnapshot) {
+      querySnapshot.forEach((doc) => {
+        var card1 = `<div class="card ">
+        <img class="card-img-top" src="${doc.data().PosterURL}" alt=""> <div class="card-body">
+        <h4 class="card-title">${doc.data().Title} - ${doc.data().Year} </h4>
+         <p class="card-text">${doc.data().Type}</p>
+      </div>
+       
+    </div>`;
+        $("#fantasy").append(card1);
+
+      });
+    })
+    .catch(function (error) {
+      console.log("Error getting documents: ", error);
+    });
+
+  db.collection("DetailMovie").where("Type", "==", "ดราม่า")
+    .get()
+    .then(function (querySnapshot) {
+      querySnapshot.forEach((doc) => {
+        var card1 = `<div class="card ">
+        <img class="card-img-top" src="${doc.data().PosterURL}" alt=""> <div class="card-body">
+        <h4 class="card-title">${doc.data().Title} - ${doc.data().Year} </h4>
+         <p class="card-text">${doc.data().Type}</p>
+      </div>
+       
+    </div>`;
+        $("#drama").append(card1);
+
+      });
+    })
+    .catch(function (error) {
+      console.log("Error getting documents: ", error);
+    });
+
+    db.collection("DetailMovie").where("Type", "==", "สยองขวัญ")
+    .get()
+    .then(function (querySnapshot) {
+      querySnapshot.forEach((doc) => {
+        var card1 = `<div class="card ">
+        <img class="card-img-top" src="${doc.data().PosterURL}" alt=""> <div class="card-body">
+        <h4 class="card-title">${doc.data().Title} - ${doc.data().Year} </h4>
+         <p class="card-text">${doc.data().Type}</p>
+      </div>
+       
+    </div>`;
+        $("#้horror").append(card1);
+
+      });
+    })
+    .catch(function (error) {
+      console.log("Error getting documents: ", error);
+    });
+
+
+    db.collection("DetailMovie").where("Type", "==", "แอคชั่น")
+    .get()
+    .then(function (querySnapshot) {
+      querySnapshot.forEach((doc) => {
+        var card1 = `<div class="card ">
+        <img class="card-img-top" src="${doc.data().PosterURL}" alt=""> <div class="card-body">
+        <h4 class="card-title">${doc.data().Title} - ${doc.data().Year} </h4>
+         <p class="card-text">${doc.data().Type}</p>
+      </div>
+       
+    </div>`;
+        $("#้action").append(card1);
+
+      });
+    })
+    .catch(function (error) {
+      console.log("Error getting documents: ", error);
+    });
+
+  
+})
   //ใส่คำอธิบาย
- 
