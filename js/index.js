0ebfaf0808b264808915a9e0f099e4d00c9b2203
Suttawee Prom-in <kooku190642@gmail.com>
2020-10-18 16:22:05 +0700

    update signin

--- js/index.js
+++ js/index.js
@@ -1,46 +1,36 @@
-$(function(){
+$(function () {
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  var firebaseConfig = {
-    apiKey: "AIzaSyB_zpLZaKVozmkmP3jpDq_ho4R7ScHvVlc",
-    authDomain: "psu1-80756.firebaseapp.com",
-    databaseURL: "https://psu1-80756.firebaseio.com",
-    projectId: "psu1-80756",
-    storageBucket: "psu1-80756.appspot.com",
-    messagingSenderId: "508447784420",
-    appId: "1:508447784420:web:3e7b55a6df7436fa235bf1",
-    measurementId: "G-2JDTBKQMDS"
-  };
-  // Initialize Firebase
-  //firebase.initializeApp(firebaseConfig);
-  //firebase.analytics();
-    firebase.auth().onAuthStateChanged(function(user) {
-      if (user) {
-        // User is signed in.
 
-        displayName = user.displayName;
-        email = user.email;
-        photoUrl = user.photoURL;
-        console.log(displayName, email, photoUrl);
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      // User is signed in.
 
-        $("#username").text(email);
-        $("#displayname").text(displayName);
-        $("#photo").attr("src",photoUrl);
+      displayName = user.displayName;
+      email = user.email;
+      photoUrl = user.photoURL;
+      console.log(displayName, email, photoUrl);
 
-        
-      } else {
-        // User is signed out.
-        window.location.href ="signin.html";
-      }
-    });
-    $("#signout").click(function () {
-      firebase.auth().signOut()
-          .then(function () {
-              // Sign-out successful.
-          }).catch(function (error) {
-              // An error happened.
-          });
+      $("#username").text(email);
+      $("#displayname").text(displayName);
+      $("#photo").attr("src", photoUrl);
+
+    } else {
+      // User is signed out.
+      window.location.href = "signin.html";
+    }
   });
-})
+  document.addEventListener('init', function (event) {
+    var page = event.target;
+    if (page.id === 'profile') {
+      $("#signOut").click(function () {
+        console.log("Exit");
+        firebase.auth().signOut().then(function () {
+          window.location.href = "signin.html"
+        }).catch(function (error) {
+            // An error happened.
+        });
+      });
+    }
+  });
 
+})
--- js/signin.js
+++ js/signin.js
@@ -1,26 +1,21 @@
 $(function(){
+  
   var firebaseConfig = {
-      apiKey: "AIzaSyB_zpLZaKVozmkmP3jpDq_ho4R7ScHvVlc",
-      authDomain: "psu1-80756.firebaseapp.com",
-      databaseURL: "https://psu1-80756.firebaseio.com",
-      projectId: "psu1-80756",
-      storageBucket: "psu1-80756.appspot.com",
-      messagingSenderId: "508447784420",
-      appId: "1:508447784420:web:3e7b55a6df7436fa235bf1",
-      measurementId: "G-2JDTBKQMDS"
-    };
-   
-
+    apiKey: "AIzaSyBfgwFZDoLIRIEIbqayesMIsk4C2BUlvvk",
+    authDomain: "mongduu-cae06.firebaseapp.com",
+    databaseURL: "https://mongduu-cae06.firebaseio.com",
+    projectId: "mongduu-cae06",
+    storageBucket: "mongduu-cae06.appspot.com",
+    messagingSenderId: "19158763880",
+    appId: "1:19158763880:web:1ba86090c57aa8edd5aedd",
+    measurementId: "G-D1SS39EJBZ"
+  };
+  // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
   firebase.analytics();
 
-  firebase.auth().onAuthStateChanged(function(user) {
-      if (user) {
-        console.log(user);
-       window.location.href = 'views/home(recommened).html';
-      }
-    });
 
+ 
   $("#signinemail").click(function(){
       var email = $("#email").val();
       var password = $("#password").val();
@@ -32,9 +27,38 @@ $(function(){
           // Handle Errors here.
           var errorCode = error.code;
           var errorMessage = error.message;
-          $("#error").text(errorMessage);
+          ons.notification.alert(errorMessage);
         });
         
    });
-  })
+   
+      firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        console.log(user);
+       window.location.href = 'mongduu.html';
+      }
+    });
+
+    $("#signingoogle").click(function(){
+    
+      var provider = new firebase.auth.GoogleAuthProvider();
+      provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+      firebase.auth().signInWithRedirect(provider);
+      firebase.auth().getRedirectResult().then(function(result) {
+          if (result.credential) {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            var token = result.credential.accessToken;
+          }
+          // The signed-in user info.
+          var user = result.user;
+        }).catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          console.log(errorCode);
+          ons.notification.alert(errorMessage);
+        });
+
+  });
+})
  
